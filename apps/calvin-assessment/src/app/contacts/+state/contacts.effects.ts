bc0317b7888504ba65e0c9ec3d7b1a6959eb506d
Calvin Durrant <calvindurrant@gmail.com>
2019-08-30 16:44:44 +0200

    not using data persistance

--- apps/calvin-assessment/src/app/contacts/+state/contacts.effects.ts
+++ apps/calvin-assessment/src/app/contacts/+state/contacts.effects.ts
@@ -1,42 +1,39 @@
 import { Injectable } from '@angular/core';
-import { Effect } from '@ngrx/effects';
-import { DataPersistence } from '@nrwl/angular';
-import { map } from 'rxjs/operators';
+import { Effect, createEffect } from '@ngrx/effects';
+import { map, catchError, mergeMap } from 'rxjs/operators';
 
-import { ContactsPartialState } from './contacts.reducer'
 import {
-  LoadContacts,
   ContactsLoaded,
   ContactsLoadError,
   ContactsActionTypes
 } from './contacts.actions';
 import { ContactsService } from '../contacts.service';
+import { Actions } from '@ngrx/effects';
+import { ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
 
 @Injectable()
 export class ContactsEffects {
-  @Effect() loadContacts$ = this.dataPersistence.fetch(
-    ContactsActionTypes.LoadContacts,
-    {
-      run: (action: LoadContacts, state: ContactsPartialState) => {
-        return this.contactsService.getContacts().pipe(
+  @Effect() loadContacts$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(ContactsActionTypes.LoadContacts),
+      mergeMap(action =>
+        this.contactsService.getContacts().pipe(
           map(res =>
             res.map(item => ({
               id: item.payload.doc.id,
               ...item.payload.doc.data()
             }))
           ),
-          map(data => new ContactsLoaded(data))
-        );
-      },
-
-      onError: (action: LoadContacts, error) => {
-        return new ContactsLoadError(error);
-      }
-    }
+          map(data => new ContactsLoaded(data)),
+          catchError(error => of(new ContactsLoadError(error)))
+        )
+      )
+    )
   );
 
   constructor(
-    private dataPersistence: DataPersistence<ContactsPartialState>,
+    private actions$: Actions,
     private contactsService: ContactsService
   ) {}
 }
--- apps/calvin-assessment/src/app/pages/pages.module.ts
+++ apps/calvin-assessment/src/app/pages/pages.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ContactsComponent } from './contacts/contacts.component';
 import { MaterialsModule } from '../materials/materials.module';
 import { ContactDetailComponent } from './contact-detail/contact-detail.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
@@ -11,6 +12,7 @@ import { ContactDetailComponent } from './contact-detail/contact-detail.componen
   imports: [
     CommonModule,
     MaterialsModule,
+    AppRoutingModule,
   ]
 })
 export class PagesModule { }
