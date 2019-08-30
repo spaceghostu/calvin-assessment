e9b574176db03eb3cbcca6714244c706e01a102b
Calvin Durrant <calvindurrant@gmail.com>
2019-08-30 10:50:38 +0200

    feat(AppRouting): added app routing module

--- apps/calvin-assessment/src/app/app.module.ts
+++ apps/calvin-assessment/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MaterialsModule } from './materials/materials.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MaterialsModule],
+  imports: [BrowserModule, MaterialsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
