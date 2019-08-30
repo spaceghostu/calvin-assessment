135d0ff6b7a85352c9dd2f7080a6fe2d0791dbed
Calvin Durrant <calvindurrant@gmail.com>
2019-08-30 15:43:13 +0200

    added loader/error on contacts page

--- apps/calvin-assessment/src/app/pages/contacts/contacts.component.ts
+++ apps/calvin-assessment/src/app/pages/contacts/contacts.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { ContactsState, Entity } from '../../contacts/+state/contacts.reducer';
 import { Observable } from 'rxjs';
-import { ContactsActionTypes, LoadContacts } from '../../contacts/+state/contacts.actions';
+import { LoadContacts } from '../../contacts/+state/contacts.actions';
 import { contactsQuery } from '../../contacts/+state/contacts.selectors';
 
 @Component({
@@ -12,14 +12,22 @@ import { contactsQuery } from '../../contacts/+state/contacts.selectors';
 })
 export class ContactsComponent implements OnInit {
 
-  contacts$: Observable<Entity[]>
+  contacts$: Observable<Entity[]>;
+  loaded$: Observable<boolean>;
+  error$: Observable<string>;
 
   constructor(private store: Store<ContactsState>) {
     this.contacts$ = this.store.select(contactsQuery.getAllContacts);
+    this.loaded$ = this.store.select(contactsQuery.getLoaded);
+    this.error$ = this.store.select(contactsQuery.getError);
     this.store.dispatch(new LoadContacts());
   }
 
   ngOnInit() {
+  }
+
+  handleRetry() {
+    this.store.dispatch(new LoadContacts());
   }
 
 }
