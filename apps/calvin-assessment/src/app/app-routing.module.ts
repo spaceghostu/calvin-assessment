7757de5446994ce68b6c40cabc46173352cc86bb
Calvin Durrant <calvindurrant@gmail.com>
2019-08-30 16:07:19 +0200

    added conatct detail page

--- apps/calvin-assessment/src/app/app-routing.module.ts
+++ apps/calvin-assessment/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactsComponent } from './pages/contacts/contacts.component';
+import { ContactDetailComponent } from './pages/contact-detail/contact-detail.component';
 
 const routes: Routes = [
   {
     path: 'contacts',
     component: ContactsComponent,
+  },
+  {
+    path: 'contact/:id',
+    component: ContactDetailComponent,
   },
   {
     path: '',
--- apps/calvin-assessment/src/app/pages/pages.module.ts
+++ apps/calvin-assessment/src/app/pages/pages.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactsComponent } from './contacts/contacts.component';
 import { MaterialsModule } from '../materials/materials.module';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 
 
 
 @NgModule({
-  declarations: [ContactsComponent],
+  declarations: [ContactsComponent, ContactDetailComponent],
   imports: [
     CommonModule,
     MaterialsModule,
