b19fd306ee8e8e46454f0d3bdb420c895dd56dbf
Calvin Durrant <calvindurrant@gmail.com>
2019-09-02 16:28:59 +0200

    fix transition

--- apps/calvin-assessment/src/app/pages/animations/pageTransition.ts
+++ apps/calvin-assessment/src/app/pages/animations/pageTransition.ts
@@ -7,17 +7,17 @@ import {
   } from '@angular/animations';
   
   const DURATION = 300; // ms
+  const DELAY = 0; // ms
   const EASING = 'cubic-bezier(.46, 1, .25, 1)';
 
-//   const ENTER_EASING = 'cubic-bezier(1,0,.5,1)';
-//   const EXIT_EASING = 'cubic-bezier(.5,0,.66,1)';
-  
-  const ENTER_TIMING = `${DURATION}ms ${EASING}`;
+  const ENTER_TIMING = `${DURATION}ms ${DELAY}ms ${EASING}`;
   const EXIT_TIMING = `${DURATION}ms ${EASING}`;
-  
+
   const BASE = {
-    position: 'relative',
-  };
+    position: 'absolute',
+    width: '100vw',
+  }
+  
   const CENTER = {
     left: '0vw',
   };
@@ -30,21 +30,13 @@ const RIGHT = {
   
   export const pageTransition = trigger('pageTransition', [
     state('*', style(BASE)),
-    transition('void => reverse', [
-        style(LEFT),
-        animate(ENTER_TIMING, style(CENTER)),
-    ]),
-    transition('reverse => void', [
+    transition(':leave', [
         style(CENTER),
-        animate(ENTER_TIMING, style(RIGHT)),
+        animate(EXIT_TIMING, style(LEFT)),
     ]),
-    transition('void => *', [
+    transition(':enter', [
         style(RIGHT),
         animate(ENTER_TIMING, style(CENTER)),
-    ]),
-    transition('* => void', [
-        style(CENTER),
-        animate(ENTER_TIMING, style(LEFT)),
     ]),
   ]);
   
